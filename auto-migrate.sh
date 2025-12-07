0a8de5b2e1067e6685b7db2a17bbf7e14aadb471
cihuyyama <miqbalalhabib@gmail.com>
2025-12-07 12:49:33 +0700

    fix: seeder

--- auto-migrate.sh
+++ auto-migrate.sh
@@ -17,17 +17,17 @@ npx prisma db push || exit 1
 # The `npx prisma db execute` command runs a raw SQL query.
 # We're checking for the existence of the `User` table. You can change this to any
 # table you know is created by your seed script.
-if ! npx prisma db execute --schema="prisma/schema.prisma" --url="$DATABASE_URL" --stdin <<EOF
-  SELECT 1 FROM \`User\` LIMIT 1;
-EOF
-then
-  # If the check fails (i.e., the table doesn't exist), then we run the seed command.
-  echo "Running Prisma database seeding..."
-  npx prisma db seed || exit 1
-else
-  # If the table exists, we assume the database has already been seeded.
-  echo "Database already seeded, skipping."
-fi
+# if ! npx prisma db execute --schema="prisma/schema.prisma" --url="$DATABASE_URL" --stdin <<EOF
+#   SELECT 1 FROM \`User\` LIMIT 1;
+# EOF
+# then
+#   # If the check fails (i.e., the table doesn't exist), then we run the seed command.
+#   echo "Running Prisma database seeding..."
+#   npx prisma db seed || exit 1
+# else
+#   # If the table exists, we assume the database has already been seeded.
+#   echo "Database already seeded, skipping."
+# fi
 
 # Echo a message to indicate that the application is starting.
 echo "Starting application..."
